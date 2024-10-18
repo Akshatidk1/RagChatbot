65d89b40e128763c196c8a1c8bedf330b2bd574f
Akshat Kumar Nishad <akshat.nishad@oges.info>
2024-10-18 13:07:35 +0530

    change in url

---
+++ utils/config.py
@@ -0,0 +1,5 @@
+LANGCHAIN_API_KEY = "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"
+LANGCHAIN_ENDPOINT = "https://api.smith.langchain.com"
+LANGCHAIN_PROJECT = "RAG_With_Memory"
+GOOGLE_API_KEY = "AIzaSyAIujOVkqnEXrz7Yj1ztWVfdw_RVWwwVGw"
+PINECONE_API_KEY = "4158a78f-e1ed-4f88-9ad6-923ee500456e"
